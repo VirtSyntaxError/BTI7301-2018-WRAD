43ce2eca31aee0428a6b10abf1a0f068e4d09edb
Philipp Köfer <philipp.koefer@isolutions.ch>
2018-10-12 11:01:10 +0200

    Upload GetADUsers IST v1

---
+++ doc/GetADUsers.ps1
@@ -0,0 +1,48 @@
+﻿
+function Get-ADNestedGroupMembership
+{
+	Param(
+		[Parameter(Mandatory=$true)]
+		[String]$strADObject,
+		[String[]]$parents = @()
+	)
+	
+	foreach ($group in Get-ADPrincipalGroupMembership -Identity:$strADObject)
+	{
+		if ($parents -inotcontains $group.DistinguishedName)
+		{
+			Write-Output $group.Name
+			Get-ADNestedGroupMembership -strADObject:$group.DistinguishedName -parents:($parents + $group.DistinguishedName)
+		}
+	}
+}
+
+
+function GetADUsers
+{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String]$filter, 
+        [String]$searchbase
+    )
+    
+    Get-ADUser -Filter:$filter -SearchBase:$searchbase -Properties:* | ForEach-Object {
+    $user = $_
+    $samAccountName = $_.SamAccountName
+	$parents = Get-ADPrincipalGroupMembership -Identity:$_.DistinguishedName
+	$parentNames = $parents | Select-Object -ExpandProperty 'name'
+	foreach ($parent in $parents)
+	{
+		$parentNames += Get-ADNestedGroupMembership -strADObject:($parent.DistinguishedName) -parents:($parents | Select-Object -ExpandProperty 'DistinguishedName')
+	}
+    $user.AllGroups = $parentNames
+    # $user | Add-Member -NotePropertyName "AllGroups" -NotePropertyValue $parentNames | Select *
+	$user | Select *
+    #$parentNames | Select-Object -Unique 
+}
+
+
+}
+
+#Example function call
+GetADUsers -filter 'Name -like "*Archer"' -searchbase "OU=WRAD,DC=WRAD,DC=local"
