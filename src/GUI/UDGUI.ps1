3caea6ef6684c23df56639df5f246663f3144d3f
michn2 <michn2@bfh.ch>
2018-10-16 19:46:09 +0200

    Update src/GUI/UDGUI.ps1

--- src/GUI/UDGUI.ps1
+++ src/GUI/UDGUI.ps1
@@ -7,7 +7,7 @@ $LogonList = @()
 #Use LastLogonTimestamp when multiple DCs are in use!
 #$OlderUser = (Get-ADUser -Filter * -Properties LastLogonTimestamp | where {$_.LastLogonTimestamp -lt (get-date).AddDays(-90).ToInt64()}).Count
 $InactiveUser = (Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 90 |measure ).count
-$DeactivatedUser = (Search-ADAccount -AccountDisabled -UsersOnly - SearchBase $SearchBase |measure).count
+$DeactivatedUser = (Search-ADAccount -AccountDisabled -UsersOnly -SearchBase $SearchBase |measure).count
 $ExpiredUser = (Search-ADAccount -AccountExpired -UsersOnly |measure).count
 $AllUser = (Get-ADUser -Filter * |measure).count
 $ActiveUser = $AllUser - $InactiveUser
@@ -43,15 +43,15 @@ $ArrAL_RF = @(
 
 $ArrAL_LC = @(
 	New-Object PSObject	-Property @{step="Warnung"; date="2018-01-01"; descr="User1 aus Gruppe 1 entfernt."}
-	New-Object PSObject	-Property @{step="Warnung"; date="2018-01-01"; descr="User1 zu Gruppe 2 hinzugefügt."}
-	New-Object PSObject	-Property @{step="Information"; date="2018-01-02"; descr="User2 wurde in WRAD hinzugefügt."}
+	New-Object PSObject	-Property @{step="Warnung"; date="2018-01-01"; descr="User1 zu Gruppe 2 hinzugefuegt."}
+	New-Object PSObject	-Property @{step="Information"; date="2018-01-02"; descr="User2 wurde in WRAD hinzugefuegt."}
 
 )
 
 $ArrAL_FpM = @(
 	New-Object PSObject	-Property @{month="Jan."; count=4}
 	New-Object PSObject	-Property @{month="Feb."; count=8}
-	New-Object PSObject	-Property @{month="März"; count=5}
+	New-Object PSObject	-Property @{month="Maerz"; count=5}
 	New-Object PSObject	-Property @{month="Apr."; count=0}
 	New-Object PSObject	-Property @{month="Juni"; count=11}
 )
@@ -62,6 +62,13 @@ $ADUserLLOt90 = (Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 90 |meas
 $ADUserLLb30a90 = (Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 30 |measure ).count - $ADUserLLOt90
 #Active User
 $ADUserActive = (Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 0 |measure ).count - $ADUserLLb30a90
+
+$ADUserActivity = @(
+    New-Object PSObject	-Property @{descr="Aelter als 90 Tage"; count=3; bckgrnd="#80ff0000"}
+    New-Object PSObject	-Property @{descr="Zwischen 30 und 90 Tagen"; count=10; bckgrnd="#80ffff00"}
+    New-Object PSObject	-Property @{descr="Aktive Benutzer"; count=87; bckgrnd="#8000ff00"}
+)
+
 
 $auth = New-UDAuthenticationMethod -Endpoint {
 	param([PSCredential]$Credentials)
@@ -80,18 +87,19 @@ $auth = New-UDAuthenticationMethod -Endpoint {
 $login = new-UDLoginPage -AuthenticationMethod $auth -WelcomeText "Welcome to WRAD" -PageBackgroundColor "white" -LoginFormFontColor "black" -LoginFormBackgroundColor "grey"
 
 $PageALDashboard = New-UDPage -Name "Abteilungsleiter" -Content {
-	New-UDHeading -Content {
+    New-UDHeading -Content {
 		New-UDCard -Endpoint { $User }
 	}
-	
+    
 	New-UDRow {
-		New-UDColumn -size 3 -Content {
+		New-UDColumn -size 6 -Content {
 			New-UdGrid -Title "Rechtefehler" -Headers @("Stufe", "Datum", "Beschreibung") -Properties @("step", "date", "descr") -AutoRefresh -RefreshInterval 60 -Endpoint {
 				$ArrAL_RF | Select step,date,descr | Out-UDGridData
 			}
 		}
-		New-UDColumn -size 3 -Content {
-			New-UdGrid -Title "Letzte Änderungen" -Headers @("Stufe", "Datum", "Beschreibung") -Properties @("step", "date", "descr") -AutoRefresh -RefreshInterval 60 -Endpoint {
+    
+		New-UDColumn -size 6 -Content {
+			New-UdGrid -Title "Letzte Aenderungen" -Headers @("Stufe", "Datum", "Beschreibung") -Properties @("step", "date", "descr") -AutoRefresh -RefreshInterval 60 -Endpoint {
 				$ArrAL_LC | Select step,date,descr | Out-UDGridData
 			}
 		}
@@ -99,24 +107,32 @@ $PageALDashboard = New-UDPage -Name "Abteilungsleiter" -Content {
 	
 	New-UDRow {
 		#Letzte Anmeldung
-		New-UDColumn -size 3 -Content {
+		New-UDColumn -size 6 -Content {
 			New-UDChart -Title "Letzte Anmledung" -Type Doughnut -RefreshInterval 5 -Endpoint { 
-				New-UdChartDataset -DataProperty $ADUserLLOt90 -Label "Last logon older than 90 Days" -BackgroundColor "#80990000" -HoverBackgroundColor "#80990000"
-				New-UdChartDataset -DataProperty $ADUserLLb30a90 -Label "Last logon between 30 and 90 Days" -BackgroundColor "#80999900" -HoverBackgroundColor "#80999900"
-				New-UdChartDataset -DataProperty $ADUserActive -Label "Last logon between 30 and 90 Days" -BackgroundColor "#80009900" -HoverBackgroundColor "#80009900"
+                $ADUserActivity | Out-UDChartData -LabelProperty "descr" -Dataset @(
+				    New-UDChartDataset -DataProperty "count"  -Label "Users" -BackgroundColor "#9055AAFF" -HoverBackgroundColor "#90ffffff"
+               )
+                
 			}
 		}
 		
 		#Fehler pro Monat
-		New-UDColumn -size 3 -Content {
-			New-UDChart -Title "Letzte Anmledung" -Type bar -RefreshInterval 5 -Endpoint { 
-				
+		New-UDColumn -size 6 -Content {
+			New-UDChart -Title "Fehler pro Monat" -Type bar -RefreshInterval 5 -Endpoint { 
+				$ArrAL_FpM | Out-UDChartData -LabelProperty "month" -Dataset @(
+                    New-UDChartDataset -DataProperty "count" -Label "Fehler" -BackgroundColor "#80990000" -HoverBackgroundColor "#80ff0000"
+                )
 			}
 		}
 	}
 	
 	New-UDRow {
-		#Status und MostUsed
+		#Status 
+        New-UDColumn -size 6 -Content {
+            New-UdGrid -Title "Status" -Headers @("Beschreibung", "Anzahl") -Properties @("descr", "count") -AutoRefresh -RefreshInterval 60 -Endpoint {
+				$ADUserActivity | Select descr,count | Out-UDGridData
+			}
+        }
 	}
 }
 
