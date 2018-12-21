e15a07a0216b72352a18d09ca1544e4336deef50
michn2 <michn2@bfh.ch>
2018-12-21 10:02:46 +0100

    Bugfixing

---
+++ src/modules/WRADGetISTAndCompare.ps1
@@ -0,0 +1,18 @@
+﻿# import Reporting module
+    try
+	{
+		Write-Verbose "Loading PS Module WRADCreateReport Class"
+		Import-Module -Name ($PSScriptRoot+"\WRADGetIST.psd1")
+        Import-Module -Name ($PSScriptRoot+"\WRADUpdateReferenceObjectGUID.psd1")
+        Import-Module -Name ($PSScriptRoot+"\WRADSOLLISTVergleich.psd1")
+	}
+	catch 
+	{
+		Write-Error -Message $_.Exception.Message
+	}
+
+Write-WRADISTtoDB -verbose
+
+Update-WRADReferenceObjectGUID -verbose
+
+Invoke-WRADSOLLISTVergleich -verbose
