1dbad36f1bf312a4f19ae0e221bafec3d5643df2
michn2 <michn2@bfh.ch>
2018-12-22 20:17:23 +0100

    updated group and user history

--- src/GUI/pageGroupHistory.ps1
+++ src/GUI/pageGroupHistory.ps1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #History of Group
 $PageGroupHistory = New-UDPage -Name "Group History" -AuthorizedRole @("WRADadmin","Auditor") -AutoRefresh -RefreshInterval 30 -Content {
     #Show Group
@@ -16,7 +15,7 @@ $PageGroupHistory = New-UDPage -Name "Group History" -AuthorizedRole @("WRADadmi
                 $AllGroup = Get-WRADGroup
 
                 ForEach($Group in $AllGroup){
-                    $AllGroupGrid += @{Groupname = $Group.CommonName; GroupType = $Group.GroupType; History =(New-UDLink -Text "History" -Url "/GroupHistory/$($Group.ObjectGUID)")} 
+                    $AllGroupGrid += @{Groupname = $Group.CommonName; GroupType = $Group.GroupType; CreatedDate = ($Group.CreatedDate |get-Date -format "dd.MM.yyyy HH:mm:ss"); History =(New-UDLink -Text "History" -Url "/GroupHistory/$($Group.ObjectGUID)")} 
                 }
                 
                 $AllGroupGrid | Out-UDGridData
@@ -78,87 +77,4 @@ $PageGroupHistoryDetail = New-UDPage -URL "/GroupHistory/:grpguid" -ArgumentList
             }
         }
     }
-        
-=======
-#History of Group
-$PageGroupHistory = New-UDPage -Name "Group History" -AuthorizedRole @("WRADadmin","Auditor") -AutoRefresh -RefreshInterval 30 -Content {
-    #Show Group
-    New-UDRow {
-        New-UDColumn -Size 3 -Content {
-
-        }
-        New-UDColumn -Size 6 -Content {
-            #List All User
-            New-UDGrid -Title "All user" -Header @("Groupname", "Group Type", "Create date", "History") -Properties @("Groupname", "GroupType", "CreatedDate", "History") -Endpoint {
-                
-                $Global:WRADDBConnection = $ArgumentList[0].dbconnection
-
-                $AllGroupGrid = @()
-                $AllGroup = Get-WRADGroup
-
-                ForEach($Group in $AllGroup){
-                    $AllGroupGrid += @{Groupname = $Group.CommonName; GroupType = $Group.GroupType; History =(New-UDLink -Text "History" -Url "/GroupHistory/$($Group.ObjectGUID)")} 
-                }
-                
-                $AllGroupGrid | Out-UDGridData
-            } -ArgumentList $WRADEndpointVar
-        }
-    }
-}
-
-$PageGroupHistoryDetail = New-UDPage -URL "/GroupHistory/:grpguid" -ArgumentList $WRADEndpointVar -AuthorizedRole @("WRADadmin","Auditor") -Endpoint {
-    param($grpguid)
-
-    #load-WRADDBCommands
-    $Global:WRADDBConnection = $ArgumentList[0].dbconnection
-    $Script:Scriptpath = $ArgumentList[0].scrptroot
-    $DBConnect = $Global:WRADDBConnection
-
-    load-WRADModules
-
-    #History View of Group
-    $GroupHistory = Get-WRADHistoryOfGroup -ObjectGUID $grpguid | Sort-Object -Property VersionEndTime -Descending
-    $GroupNow = Get-WRADGroup -ObjectGUID $grpguid
-    New-UDRow {
-        New-UDColumn -Size 12 -Content {
-            New-UDHTML "<p style='color:white;font-size:1.8em'>History of Group $grpguid</p>"
-        }
-    }
-    New-UDRow {
-        New-UDColumn -Size 12 -Content {
-            New-UDCollapsible -Items {
-                
-                $DateFrom = $GroupNow.LastModifiedDate |get-Date -format "dd.MM.yyyy HH:mm:ss"
-                $DateTo = "now"
-                    New-UDCollapsibleItem -Title "$DateFrom - $DateTo" -Content {
-                         New-UDCollection -Content {
-                            New-UDCollectionItem -Content { "Groupname: "+$GroupNow.CommonName }
-                            New-UDCollectionItem -Content { "SAMAccountName: "+$GroupNow.SAMAccountName }
-                            New-UDCollectionItem -Content { "Distinguished Name: "+$GroupNow.DistinguishedName }
-                            New-UDCollectionItem -Content { "Description: "+$GroupNow.Description }
-                            New-UDCollectionItem -Content { "Group Type: "+$GroupNow.GroupType }
-                            New-UDCollectionItem -Content { "Group Type Security: "+$GroupNow.GroupTypeSecurity }
-                        }
-                    }
-
-                ForEach($Entry in $GroupHistory){
-                    $DateFrom = $Entry.VersionStartTime |get-Date -format "dd.MM.yyyy HH:mm:ss"
-                    $DateTo = $Entry.VersionEndTime |get-Date -format "dd.MM.yyyy HH:mm:ss"
-                    New-UDCollapsibleItem -Title "$DateFrom - $DateTo" -Content {
-                         New-UDCollection -Content {
-                            New-UDCollectionItem -Content { "Groupname: "+$Entry.CommonName }
-                            New-UDCollectionItem -Content { "SAMAccountName: "+$Entry.SAMAccountName }
-                            New-UDCollectionItem -Content { "Distinguished Name: "+$Entry.DistinguishedName }
-                            New-UDCollectionItem -Content { "Description: "+$Entry.Description }
-                            New-UDCollectionItem -Content { "Group Type: "+$Entry.GroupType }
-                            New-UDCollectionItem -Content { "Group Type Security: "+$Entry.GroupTypeSecurity }
-                            New-UDCollectionItem -Content { "OperationType: "+$Entry.OperationType }
-                        }
-                    }
-                }
-            }
-        }
-    }
-        
->>>>>>> 09b9f8c6de655a64a38d8fc675152a1d8f7a8e11
 }
--- src/GUI/pageUserHistory.ps1
+++ src/GUI/pageUserHistory.ps1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #History of User
 $PageUserHistory = New-UDPage -Name "User History" -AuthorizedRole @("WRADadmin","Auditor") -AutoRefresh -RefreshInterval 30 -Content {
     #Show User
@@ -7,12 +6,9 @@ $PageUserHistory = New-UDPage -Name "User History" -AuthorizedRole @("WRADadmin"
 
         }
         New-UDColumn -Size 6 -Content {
+            
             #List All User
-<<<<<<< HEAD
             New-UDGrid -Title "All user" -Header @("Username", "Displayname", "Create date", "Enabled", "History") -Properties @("Username", "DisplayName", "CreatedDate", "Enabled", "History") -Endpoint {
-=======
-            New-UDGrid -Title "All user" -Header @("Username", "Displayname", "Create date", "Enabled", "Edit") -Properties @("Username", "DisplayName", "CreatedDate", "Enabled", "Edit") -Endpoint {
->>>>>>> 6581f3e577d4a41c3d8c8e039f5dde8d03cd2c79
                 
                 $Global:WRADDBConnection = $ArgumentList[0].dbconnection
 
@@ -20,11 +16,7 @@ $PageUserHistory = New-UDPage -Name "User History" -AuthorizedRole @("WRADadmin"
                 $AllUser = Get-WRADUser
 
                 ForEach($User in $AllUser){
-<<<<<<< HEAD
-                    $AllUserGrid += @{Username = $User.UserPrincipalName; DisplayName = $User.DisplayName; Enabled = $User.Enabled; History =(New-UDLink -Text "History" -Url "/UserHistory/$($User.ObjectGUID)")} 
-=======
-                    $AllUserGrid += @{Username = $User.UserPrincipalName; DisplayName = $User.DisplayName; Enabled = $User.Enabled; Edit =(New-UDLink -Text "History" -Url "/UserHistory/$($User.ObjectGUID)")} 
->>>>>>> 6581f3e577d4a41c3d8c8e039f5dde8d03cd2c79
+                    $AllUserGrid += @{Username = $User.UserPrincipalName; DisplayName = $User.DisplayName; CreatedDate = ($User.CreatedDate |get-Date -format "dd.MM.yyyy HH:mm:ss"); Enabled = $User.Enabled; History =(New-UDLink -Text "History" -Url "/UserHistory/$($User.ObjectGUID)")} 
                 }
                 
                 $AllUserGrid | Out-UDGridData
@@ -33,14 +25,8 @@ $PageUserHistory = New-UDPage -Name "User History" -AuthorizedRole @("WRADadmin"
     }
 }
 
-<<<<<<< HEAD
 $PageUserHistoryDetail = New-UDPage -URL "/UserHistory/:usrguid" -ArgumentList $WRADEndpointVar -AuthorizedRole @("WRADadmin","Auditor") -Endpoint {
     param($usrguid)
-=======
-$PageUserHistoryDyn = New-UDPage -URL "/UserHistory/:usrguid" -ArgumentList $WRADEndpointVar -AuthorizedRole @("WRADadmin","Auditor") -Endpoint {
-    param($usrguid)
-    #Edit User details with Memberships
->>>>>>> 6581f3e577d4a41c3d8c8e039f5dde8d03cd2c79
 
     #load-WRADDBCommands
     $Global:WRADDBConnection = $ArgumentList[0].dbconnection
@@ -48,7 +34,6 @@ $PageUserHistoryDyn = New-UDPage -URL "/UserHistory/:usrguid" -ArgumentList $WRA
     $DBConnect = $Global:WRADDBConnection
 
     load-WRADModules
-<<<<<<< HEAD
 
     #History View of User
     $UserHistory = Get-WRADHistoryOfUser -ObjectGUID $usrguid | Sort-Object -Property VersionEndTime -Descending
@@ -63,18 +48,18 @@ $PageUserHistoryDyn = New-UDPage -URL "/UserHistory/:usrguid" -ArgumentList $WRA
             New-UDCollapsible -Items {
 
                 $DateFrom = $UserNow.LastModifiedDate |get-Date -format "dd.MM.yyyy HH:mm:ss"
-                    $DateTo = "now"
-                        New-UDCollapsibleItem -Title "$DateFrom - $DateTo" -Content {
-                             New-UDCollection -Content {
-                                New-UDCollectionItem -Content { "Username: "+$UserNow.userPrincipalName }
-                                New-UDCollectionItem -Content { "SAMAccountName: "+$UserNow.SAMAccountName }
-                                New-UDCollectionItem -Content { "Display Name: "+$UserNow.DisplayName }
-                                New-UDCollectionItem -Content { "Distinguished Name: "+$UserNow.DistinguishedName }
-                                New-UDCollectionItem -Content { "Description: "+$UserNow.Description }
-                                New-UDCollectionItem -Content { "Enabled: "+$UserNow.Enabled }
-                                New-UDCollectionItem -Content { "Expired: "+$UserNow.Expired }
-                            }
-                        }
+                $DateTo = "now"
+                New-UDCollapsibleItem -Title "$DateFrom - $DateTo" -Content {
+                    New-UDCollection -Content {
+                        New-UDCollectionItem -Content { "Username: "+$UserNow.userPrincipalName }
+                        New-UDCollectionItem -Content { "SAMAccountName: "+$UserNow.SAMAccountName }
+                        New-UDCollectionItem -Content { "Display Name: "+$UserNow.DisplayName }
+                        New-UDCollectionItem -Content { "Distinguished Name: "+$UserNow.DistinguishedName }
+                        New-UDCollectionItem -Content { "Description: "+$UserNow.Description }
+                        New-UDCollectionItem -Content { "Enabled: "+$UserNow.Enabled }
+                        New-UDCollectionItem -Content { "Expired: "+$UserNow.Expired }
+                    }
+                }
 
                 ForEach($Entry in $UserHistory){
                     $DateFrom = $Entry.VersionStartTime |get-Date -format "dd.MM.yyyy HH:mm:ss"
@@ -95,262 +80,4 @@ $PageUserHistoryDyn = New-UDPage -URL "/UserHistory/:usrguid" -ArgumentList $WRA
             }
         }
     }
-        
-=======
-    enable-WRADLogging
-    
-    New-UDRow {
-        New-UDColumn -Size 6 -Content {
-            #Detailed View of User
-            $Script:EUuser = Get-WRADUser -ObjectGUID $usrguid
-  
-            if($Script:EUuser.Enabled){
-                $EUenabled = "Yes"
-            } else {
-                $EUenabled = "No"
-            }
-
-        } 
-        New-UDColumn -Size 6 -Content {
-            #Membership of User
-            $Script:EUgroup = Get-WRADGroupOfUser -Reference -UserObjectGUID $usrguid
-
-            $UsrGrp = @()
-            ForEach($group in $Script:EUgroup){
-                $newgroup = Get-WRADGroup -Reference -ObjectGUID $group.GroupObjectGUID
-                
-
-                $lnkremusr = New-UDElement -Tag "a" -Attributes @{
-                    className = "btn"
-                    target = "_self"
-                    href = "$usrguid"
-                    onClick = {
-                        #Remove selected Group from Group
-                        if([string]::IsNullOrEmpty($Global:WRADDBConnection)){
-                            $Global:WRADDBConnection = $DBConnect
-                        }
-
-                        Remove-WRADGroupOfUser -Reference -UserObjectGUID $usrguid -GroupObjectGUID $newgroup.ObjectGUID
-                        Write-WRADLog -logtext "Removed User $usrguid from Group $($newgroup.CommonName)" -level 0
-                    } 
-                } -Content {"Leave"}
-
-                $UsrGrp += @{ GroupName = $newgroup.CommonName; Edit = $lnkremusr}
-            }
-            New-UDGrid -Title "Member of" -Header @("GroupName", "Edit") -Properties @("GroupName", "Edit") -Endpoint {
-                $UsrGrp | Out-UDGridData
-            }
-        } 
-    }
-    New-UDRow {
-        New-UDColumn -Size 6 -Content {
-            $allgrps = (Get-WRADGroup -Reference).ObjectGUID
-            $usringrp = (Get-WRADGroupOfUser -Reference -UserObjectGUID $usrguid).GroupObjectGUID
-
-            $filteredgrps = $allgrps | where {$usringrp -notcontains $_}
-            $grpforusr = @()
-            ForEach($Group in $filteredgrps){
-                $tg = Get-WRADGroup -Reference -ObjectGUID $Group
-                $lnkremusr = New-UDElement -Tag "a" -Attributes @{
-                    className = "btn"
-                    target = "_self"
-                    href = "$usrguid"
-                    onClick = {
-                        #Remove selected Group from Group
-                        if([string]::IsNullOrEmpty($Global:WRADDBConnection)){
-                            $Global:WRADDBConnection = $DBConnect
-                        }
-
-                        New-WRADGroupOfUser -Reference -UserObjectGUID $usrguid -GroupObjectGUID $tg.ObjectGUID
-                        Write-WRADLog -logtext "Added User $usrguid to Group $($tg.CommonName)" -level 0
-                    } 
-                } -Content {"Add"}
-
-                $grpforusr += @{CommonName = $tg.CommonName; CreatedDate = $tg.CreatedDate; Edit = $lnkremusr}
-            }
-
-            New-UDGrid -Title "Add $($Script:EUuser.DisplayName) to Group" -Header @("CommonName", "Create date", "Edit") -Properties @("CommonName", "CreatedDate", "Edit") -Endpoint {
-                $grpforusr | Out-UDGridData
-            }
-        }
-    } 
->>>>>>> 6581f3e577d4a41c3d8c8e039f5dde8d03cd2c79
-=======
-#History of User
-$PageUserHistory = New-UDPage -Name "User History" -AuthorizedRole @("WRADadmin","Auditor") -AutoRefresh -RefreshInterval 30 -Content {
-    #Show User
-    New-UDRow {
-        New-UDColumn -Size 3 -Content {
-
-        }
-        New-UDColumn -Size 6 -Content {
-            #List All User
-<<<<<<< HEAD
-            New-UDGrid -Title "All user" -Header @("Username", "Displayname", "Create date", "Enabled", "History") -Properties @("Username", "DisplayName", "CreatedDate", "Enabled", "History") -Endpoint {
-=======
-            New-UDGrid -Title "All user" -Header @("Username", "Displayname", "Create date", "Enabled", "Edit") -Properties @("Username", "DisplayName", "CreatedDate", "Enabled", "Edit") -Endpoint {
->>>>>>> 6581f3e577d4a41c3d8c8e039f5dde8d03cd2c79
-                
-                $Global:WRADDBConnection = $ArgumentList[0].dbconnection
-
-                $AllUserGrid = @()
-                $AllUser = Get-WRADUser
-
-                ForEach($User in $AllUser){
-<<<<<<< HEAD
-                    $AllUserGrid += @{Username = $User.UserPrincipalName; DisplayName = $User.DisplayName; Enabled = $User.Enabled; History =(New-UDLink -Text "History" -Url "/UserHistory/$($User.ObjectGUID)")} 
-=======
-                    $AllUserGrid += @{Username = $User.UserPrincipalName; DisplayName = $User.DisplayName; Enabled = $User.Enabled; Edit =(New-UDLink -Text "History" -Url "/UserHistory/$($User.ObjectGUID)")} 
->>>>>>> 6581f3e577d4a41c3d8c8e039f5dde8d03cd2c79
-                }
-                
-                $AllUserGrid | Out-UDGridData
-            } -ArgumentList $WRADEndpointVar
-        }
-    }
-}
-
-<<<<<<< HEAD
-$PageUserHistoryDetail = New-UDPage -URL "/UserHistory/:usrguid" -ArgumentList $WRADEndpointVar -AuthorizedRole @("WRADadmin","Auditor") -Endpoint {
-    param($usrguid)
-=======
-$PageUserHistoryDyn = New-UDPage -URL "/UserHistory/:usrguid" -ArgumentList $WRADEndpointVar -AuthorizedRole @("WRADadmin","Auditor") -Endpoint {
-    param($usrguid)
-    #Edit User details with Memberships
->>>>>>> 6581f3e577d4a41c3d8c8e039f5dde8d03cd2c79
-
-    #load-WRADDBCommands
-    $Global:WRADDBConnection = $ArgumentList[0].dbconnection
-    $Script:Scriptpath = $ArgumentList[0].scrptroot
-    $DBConnect = $Global:WRADDBConnection
-
-    load-WRADModules
-<<<<<<< HEAD
-
-    #History View of User
-    $UserHistory = Get-WRADHistoryOfUser -ObjectGUID $usrguid | Sort-Object -Property VersionEndTime -Descending
-    $UserNow = Get-WRADUser -ObjectGUID $usrguid
-    New-UDRow {
-        New-UDColumn -Size 12 -Content {
-            New-UDHTML "<p style='color:white;font-size:1.8em'>History of User $usrguid</p>"
-        }
-    }
-    New-UDRow {
-        New-UDColumn -Size 12 -Content {
-            New-UDCollapsible -Items {
-
-                $DateFrom = $UserNow.LastModifiedDate |get-Date -format "dd.MM.yyyy HH:mm:ss"
-                    $DateTo = "now"
-                        New-UDCollapsibleItem -Title "$DateFrom - $DateTo" -Content {
-                             New-UDCollection -Content {
-                                New-UDCollectionItem -Content { "Username: "+$UserNow.userPrincipalName }
-                                New-UDCollectionItem -Content { "SAMAccountName: "+$UserNow.SAMAccountName }
-                                New-UDCollectionItem -Content { "Display Name: "+$UserNow.DisplayName }
-                                New-UDCollectionItem -Content { "Distinguished Name: "+$UserNow.DistinguishedName }
-                                New-UDCollectionItem -Content { "Description: "+$UserNow.Description }
-                                New-UDCollectionItem -Content { "Enabled: "+$UserNow.Enabled }
-                                New-UDCollectionItem -Content { "Expired: "+$UserNow.Expired }
-                            }
-                        }
-
-                ForEach($Entry in $UserHistory){
-                    $DateFrom = $Entry.VersionStartTime |get-Date -format "dd.MM.yyyy HH:mm:ss"
-                    $DateTo = $Entry.VersionEndTime |get-Date -format "dd.MM.yyyy HH:mm:ss"
-                    New-UDCollapsibleItem -Title "$DateFrom - $DateTo" -Content {
-                         New-UDCollection -Content {
-                            New-UDCollectionItem -Content { "Username: "+$Entry.userPrincipalName }
-                            New-UDCollectionItem -Content { "SAMAccountName: "+$Entry.SAMAccountName }
-                            New-UDCollectionItem -Content { "Display Name: "+$Entry.DisplayName }
-                            New-UDCollectionItem -Content { "Distinguished Name: "+$Entry.DistinguishedName }
-                            New-UDCollectionItem -Content { "Description: "+$Entry.Description }
-                            New-UDCollectionItem -Content { "Enabled: "+$Entry.Enabled }
-                            New-UDCollectionItem -Content { "Expired: "+$Entry.Expired }
-                            New-UDCollectionItem -Content { "OperationType: "+$Entry.OperationType }
-                        }
-                    }
-                }
-            }
-        }
-    }
-        
-=======
-    enable-WRADLogging
-    
-    New-UDRow {
-        New-UDColumn -Size 6 -Content {
-            #Detailed View of User
-            $Script:EUuser = Get-WRADUser -ObjectGUID $usrguid
-  
-            if($Script:EUuser.Enabled){
-                $EUenabled = "Yes"
-            } else {
-                $EUenabled = "No"
-            }
-
-        } 
-        New-UDColumn -Size 6 -Content {
-            #Membership of User
-            $Script:EUgroup = Get-WRADGroupOfUser -Reference -UserObjectGUID $usrguid
-
-            $UsrGrp = @()
-            ForEach($group in $Script:EUgroup){
-                $newgroup = Get-WRADGroup -Reference -ObjectGUID $group.GroupObjectGUID
-                
-
-                $lnkremusr = New-UDElement -Tag "a" -Attributes @{
-                    className = "btn"
-                    target = "_self"
-                    href = "$usrguid"
-                    onClick = {
-                        #Remove selected Group from Group
-                        if([string]::IsNullOrEmpty($Global:WRADDBConnection)){
-                            $Global:WRADDBConnection = $DBConnect
-                        }
-
-                        Remove-WRADGroupOfUser -Reference -UserObjectGUID $usrguid -GroupObjectGUID $newgroup.ObjectGUID
-                        Write-WRADLog -logtext "Removed User $usrguid from Group $($newgroup.CommonName)" -level 0
-                    } 
-                } -Content {"Leave"}
-
-                $UsrGrp += @{ GroupName = $newgroup.CommonName; Edit = $lnkremusr}
-            }
-            New-UDGrid -Title "Member of" -Header @("GroupName", "Edit") -Properties @("GroupName", "Edit") -Endpoint {
-                $UsrGrp | Out-UDGridData
-            }
-        } 
-    }
-    New-UDRow {
-        New-UDColumn -Size 6 -Content {
-            $allgrps = (Get-WRADGroup -Reference).ObjectGUID
-            $usringrp = (Get-WRADGroupOfUser -Reference -UserObjectGUID $usrguid).GroupObjectGUID
-
-            $filteredgrps = $allgrps | where {$usringrp -notcontains $_}
-            $grpforusr = @()
-            ForEach($Group in $filteredgrps){
-                $tg = Get-WRADGroup -Reference -ObjectGUID $Group
-                $lnkremusr = New-UDElement -Tag "a" -Attributes @{
-                    className = "btn"
-                    target = "_self"
-                    href = "$usrguid"
-                    onClick = {
-                        #Remove selected Group from Group
-                        if([string]::IsNullOrEmpty($Global:WRADDBConnection)){
-                            $Global:WRADDBConnection = $DBConnect
-                        }
-
-                        New-WRADGroupOfUser -Reference -UserObjectGUID $usrguid -GroupObjectGUID $tg.ObjectGUID
-                        Write-WRADLog -logtext "Added User $usrguid to Group $($tg.CommonName)" -level 0
-                    } 
-                } -Content {"Add"}
-
-                $grpforusr += @{CommonName = $tg.CommonName; CreatedDate = $tg.CreatedDate; Edit = $lnkremusr}
-            }
-
-            New-UDGrid -Title "Add $($Script:EUuser.DisplayName) to Group" -Header @("CommonName", "Create date", "Edit") -Properties @("CommonName", "CreatedDate", "Edit") -Endpoint {
-                $grpforusr | Out-UDGridData
-            }
-        }
-    } 
->>>>>>> 6581f3e577d4a41c3d8c8e039f5dde8d03cd2c79
->>>>>>> 09b9f8c6de655a64a38d8fc675152a1d8f7a8e11
 }
